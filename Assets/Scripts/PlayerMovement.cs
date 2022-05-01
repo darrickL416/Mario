c13997da0853706337bef9a66216a123e4bf0bb4
dlittle416 <myhoboapps@gmail.com>
2022-05-01 12:16:22 -0500

    Add snail enemy movement and player walk jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D myBody;
     private Animator anim;
+    private bool isGround;
+    private bool Jumped;
+    private float jumpPower = 5f;
+    public Transform groundCheckedPosition;
+    public LayerMask groundLayer;
 
 
 
@@ -22,7 +27,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckedIfGrounded();
+        PlayerJump();
     }
 
 
@@ -39,9 +45,56 @@ public class PlayerMovement : MonoBehaviour
         if(h > 0)
         {
             myBody.velocity = new Vector2(speed, myBody.velocity.y);
+            ChangeDirection(1);
         }else if (h < 0)
         {
             myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+            ChangeDirection(-1);
+        }
+        else
+        {
+            myBody.velocity = new Vector2(0f, myBody.velocity.y);
+        }
+
+        anim.SetInteger("Speed", Mathf.Abs((int)myBody.velocity.x));
+
+
+    }
+
+    void ChangeDirection(int direction)
+    {
+        Vector3 tempScale = transform.localScale;
+        tempScale.x = direction;
+        transform.localScale = tempScale;
+    }
+
+    void CheckedIfGrounded()
+    {
+        
+     isGround = Physics2D.Raycast(groundCheckedPosition.position, Vector2.down, 0.1f, groundLayer);
+        if (isGround)
+        {
+            if (Jumped)
+            {
+                Jumped = false;
+                anim.SetBool("Jump", false);
+            }
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (isGround)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jumped = true;
+                myBody.velocity = new Vector2(myBody.velocity.x, jumpPower);
+
+                anim.SetBool("Jump",true);
+                    
+
+            }
         }
     }
 
---
+++ Assets/Scripts/SnailScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailScript : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private bool moveLeft;
+
+    public Transform down_Collision;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        moveLeft = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveLeft)
+        {
+            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+        }
+        else
+        {
+            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+        }
+
+        CheckedCollision();
+    }
+
+    void CheckedCollision()
+    {
+        //if we don't detect collision
+        if(!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
+        {
+            ChangeDirection();
+        }
+    }
+
+
+    void ChangeDirection()
+    {
+        moveLeft = !moveLeft;
+        Vector3 tempScale = transform.localScale;
+
+        if (moveLeft)
+        {
+            tempScale.x = Mathf.Abs(tempScale.x);
+
+        }
+        else
+        {
+            tempScale.x = -Mathf.Abs(tempScale.x);
+        }
+
+        transform.localScale = tempScale;
+    }
+
+}//class
