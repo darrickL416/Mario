027030cbc6f6077d264d40e390d1e77d048a0176
dlittle416 <myhoboapps@gmail.com>
2022-05-03 20:29:11 -0500

    bird attack

--- Assets/Scripts/MyTags.cs
+++ Assets/Scripts/MyTags.cs
@@ -6,4 +6,7 @@ public class MyTags : MonoBehaviour
 {
     public static string PLAYER_TAG = "Player";
     public static string BEETLE_TAG = "beetle";
+    public static string SNAIL_TAG = "snail";
+    public static string BULLET_TAG = "bullet";
+
 }
